c1309925b30076de8e67536aa76b362709875c4d
TechSimplifide <ry0805502@gmail.com>
2026-08-27 16:59:21 +0530

    fix: configure dynamic authentication cookies

--- backend/src/constants/cookieOptions.js
+++ backend/src/constants/cookieOptions.js
@@ -1,14 +1,13 @@
-
 export const accessTokenCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   maxAge: 24 * 60 * 60 * 1000, // 1 day
 };
 
 export const refreshTokenCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
 };
