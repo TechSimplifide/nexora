630559db95a097dfe70ac22f6dd3c0e365c4ca40
TechSimplifide <ry0805502@gmail.com>
2026-09-13 13:48:47 +0530

    fix: add rate limiter to college routes

--- backend/src/routes/college.routes.js
+++ backend/src/routes/college.routes.js
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { registerCollege } from "../controllers/college.controller.js";
 import validate from "../middlewares/validate.middleware.js";
 import { collegeRegisterSchema } from "../validators/college.validator.js";
+import { authLimiter } from "../middlewares/rate-limit.middleware.js";
 
 const router = Router();
 
 router
   .route("/college/register")
-  .post(validate(collegeRegisterSchema), registerCollege);
+  .post(authLimiter, validate(collegeRegisterSchema), registerCollege);
 
 export default router;
